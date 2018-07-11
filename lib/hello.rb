15428678156fb5806e7f79af6debe9145fa5e04d
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-11 01:04:43 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,24 @@
+# def hello_t(names)
+#   if names.length == 0
+#     puts "Hey! No block was given!"
+#   else
+#     names.each { |name| puts name }
+#   end
+# end
+
 def hello_t(names)
-  if names.length == 0
-    puts "Hey! No block was given!"
-  else
-    names.each { |name| puts name }
+  i = 0
+
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+
+  names
+end
+# call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
   end
 end
-
-# call your method here!
